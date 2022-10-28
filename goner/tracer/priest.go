a2deab07aacf8a9eb5b8b72687c1835236162207
degfy <dapeng@nimei>
2022-10-28 21:55:22 +0800

    feat: 优化tracer的实现

--- goner/tracer/priest.go
+++ goner/tracer/priest.go
@@ -2,15 +2,7 @@ package tracer
 
 import (
 	"github.com/gone-io/gone"
-	"github.com/jtolds/gls"
 )
-
-func NewTracer() (gone.Goner, gone.GonerId) {
-	return &tracer{
-		mgr:        gls.NewContextManager(),
-		traceIdKey: gls.GenSym(),
-	}, gone.IdGoneTracer
-}
 
 func Priest(cemetery gone.Cemetery) error {
 	if nil == cemetery.GetTomById(gone.IdGoneTracer) {
