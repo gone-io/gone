b60b5d85c7aea199e07277d7a0a18c648acd9edc
dapeng <wangju@openviewtech.com>
2024-05-09 10:46:34 +0800

    example： use config

---
+++ example/use-config/go.mod
@@ -0,0 +1,16 @@
+module use-config
+
+go 1.21.1
+
+require github.com/gone-io/gone v0.1.4
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
+
+replace github.com/gone-io/gone v0.1.4 => ../..
---
+++ example/use-config/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gone-io/gone"
+	"github.com/gone-io/gone/goner/config"
+	"time"
+)
+
+type SubConf struct {
+	X string `properties:"x"`
+	Y string `properties:"y"`
+}
+
+type UseConfig struct {
+	gone.Flag
+
+	int      int           `gone:"config,my.conf.int"`
+	float64  float64       `config:"config,my.conf.float64"`
+	string   string        `gone:"config,my.conf.string"`
+	bool     bool          `gone:"config,my.conf.bool"`
+	duration time.Duration `gone:"config,my.conf.duration"`
+	defaultV string        `gone:"config,my.conf.default,default=ok"`
+
+	sub *SubConf `gone:"config,my.conf.sub"`
+}
+
+func (g *UseConfig) AfterRevive() gone.AfterReviveError {
+	fmt.Printf("int=%d\n", g.int)
+	fmt.Printf("float64=%f\n", g.float64)
+	fmt.Printf("string=%s\n", g.string)
+	fmt.Printf("bool=%t\n", g.bool)
+	fmt.Printf("duration=%v\n", g.duration)
+	fmt.Printf("defaultV=%s\n", g.defaultV)
+	fmt.Printf("sub.x=%v\n", g.sub)
+
+	return nil
+}
+
+func main() {
+	gone.Run(func(cemetery gone.Cemetery) error {
+		_ = config.Priest(cemetery)
+		cemetery.Bury(&UseConfig{})
+		return nil
+	})
+}
