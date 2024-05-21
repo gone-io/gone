6dd822830ec87aca4571bf70a069a03b8813888e
dapeng <wangju@openviewtech.com>
2024-05-22 07:01:42 +0800

    test: for goner/urllib/req.go

--- goner/urllib/req.go
+++ goner/urllib/req.go
@@ -18,18 +18,20 @@ type r struct {
 	tracer.Tracer `gone:"gone-tracer"`
 }
 
+func (r *r) trip(rt req.RoundTripper) req.RoundTripFunc {
+	return func(req *req.Request) (resp *req.Response, err error) {
+		tracerId := r.GetTraceId()
+		//传递traceId
+		req.SetHeader(TraceIdHeaderKey, tracerId)
+		resp, err = rt.RoundTrip(req)
+		return
+	}
+}
+
 func (r *r) AfterRevive() gone.AfterReviveError {
 	r.Client = req.C()
 
-	r.Client.WrapRoundTripFunc(func(rt req.RoundTripper) req.RoundTripFunc {
-		return func(req *req.Request) (resp *req.Response, err error) {
-			tracerId := r.GetTraceId()
-			//传递traceId
-			req.SetHeader(TraceIdHeaderKey, tracerId)
-			resp, err = rt.RoundTrip(req)
-			return
-		}
-	})
+	r.Client.WrapRoundTripFunc(r.trip)
 
 	return nil
 }
---
+++ goner/urllib/req_test.go
@@ -0,0 +1,49 @@
+package urllib
+
+import (
+	"github.com/golang/mock/gomock"
+	"github.com/gone-io/gone"
+	"github.com/gone-io/gone/goner/config"
+	"github.com/gone-io/gone/goner/logrus"
+	"github.com/gone-io/gone/goner/tracer"
+	"github.com/imroc/req/v3"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_r_AfterRevive(t *testing.T) {
+	gone.Prepare(tracer.Priest, logrus.Priest, config.Priest).AfterStart(func(in struct {
+		tracer tracer.Tracer `gone:"gone-tracer"`
+	}) {
+		g := r{
+			Tracer: in.tracer,
+		}
+		err := g.AfterRevive()
+		assert.Nil(t, err)
+	}).Run()
+}
+
+func Test_r_trip(t *testing.T) {
+	gone.Prepare(tracer.Priest, logrus.Priest, config.Priest).AfterStart(func(in struct {
+		tracer tracer.Tracer `gone:"gone-tracer"`
+	}) {
+		controller := gomock.NewController(t)
+		defer controller.Finish()
+
+		tripper := NewMockRoundTripper(controller)
+
+		in.tracer.SetTraceId("xxxx", func() {
+			tripper.EXPECT().RoundTrip(gomock.Any()).Do(func(req *req.Request) {
+				traceId := req.Headers.Get(TraceIdHeaderKey)
+				assert.Equal(t, "xxxx", traceId)
+			}).Return(nil, nil)
+
+			g := r{
+				Tracer: in.tracer,
+			}
+			trip := g.trip(tripper)
+			_, err := trip(&req.Request{})
+			assert.Nil(t, err)
+		})
+	}).Run()
+}
